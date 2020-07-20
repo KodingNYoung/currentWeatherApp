08b9ff3a25d2e4b7c8645e27cc776d0656ad8862
Young <abiodunadebambo44@gmail.com>
2020-07-20 14:37:14 +0100

    fixed URL issue

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,7 @@
 class Weather{
     constructor () {
         this.APIkey = "d2ea27e8a86a94f7542a96aa155da6e2";
-        this.APIurl = "http://api.openweathermap.org/data/2.5/weather"; 
+        this.APIurl = "https://api.openweathermap.org/data/2.5/weather"; 
     }
 
     async fetchWeatherUpdate (city, country) {
