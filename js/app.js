a1b552a82c3acbc00c8b6f0e683da737d11620ad
Young <abiodunadebambo44@gmail.com>
2020-07-27 04:51:46 +0100

    add a load on open feature

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ const ui = new UI();
 // init weather
 const weather = new Weather();
 
+// init stoarge
+const storage = new Storage();
+
 // UI ELEMENTS
 // get form display btn
 const formDisplayBtn = document.getElementById("change-loc-btn");
@@ -28,6 +31,24 @@ formHideBtn.addEventListener("click", () => {
     ui.hideForm();
 })
 
+// on the document when ever the browser is refreshed
+document.addEventListener("DOMContentLoaded", ()=> {
+    console.log("content loaded")
+    // check if there is any saved location
+    if (storage.isLocationSaved()){
+        console.log("location was stored, restoring location info")
+        // get the stored location and send the request
+        const location = storage.getStoredLocation();
+
+        const city = location.city;
+        const country = location.country;
+
+        handleLocation({city, country});
+    }else {
+        // if not auto update location
+        autoUpdateLocation();
+    }
+}) 
 // to the update btn
 updateBtn.addEventListener("click", (e) => {
     e.preventDefault();
@@ -42,20 +63,23 @@ updateBtn.addEventListener("click", (e) => {
     handleLocation({city,country});
 })
 
-// to the auto update btn
-autoUpdateBtn.addEventListener("click",async () => {
+
+const autoUpdateLocation = async () => {
+    console.log("auto-updating")
     // init Location object
     const location = new Location();
-
+    
     // get the coordinates
     const locationID = await location.getCoords();
-
+    
     const long = locationID.long;
     const lat = locationID.lat;
-
+    
     handleLocation({lat, long})    
-})
+}
 
+// to the auto update btn
+autoUpdateBtn.addEventListener("click", autoUpdateLocation)
 
 const handleLocation = (location) => {
     const long = location.long;
@@ -70,28 +94,33 @@ const handleLocation = (location) => {
             // show alert
             ui.showNoCityToast(val.message, "no-city");
         }else{
+            // parse to local storage
+            storage.setLocationToStorage(val.name, val.sys.country)
+
             // display it
             ui.updateUI(val)
         }
     }
 
     // console.log("CITY before here", city)
-    if (city){
+    if (lat && long){
+        // fetch the weather updates for the coordinates
+        update = weather.fetchWeatherUpdateByCoords(lat, long);
+    }else  {
         // console.log("here", city)
         // if city input is empty show alert
-        if (city === "") {
+        if (!city) {
             ui.showFormAlert("City is required", "");
+            return
         }else{
             // fetch the weather updates for the location
             update = weather.fetchWeatherUpdateByCity(city, country);
 
-            console.log(update);
+            // console.log(update);
         }
-    }else if (lat && long) {
-        // fetch the weather updates for the coordinates
-        update = weather.fetchWeatherUpdateByCoords(lat, long); 
     }
 
+    // console.log(update)
     update.then(val => handleWeatherUpdate(val))
             
     .catch((err) => console.log(err))
